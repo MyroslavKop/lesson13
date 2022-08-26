ded4630b9d522cf2d2e320a308a609ce3417cfd2
MiroslavKop <kopytovmiroslav@gmail.com>
2022-08-26 21:52:58 +0300

    вариант async/await

--- script.js
+++ script.js
@@ -4,17 +4,39 @@ const API_URL = "https://jsonplaceholder.typicode.com/users";
 
 // then.catch.----------------------/
 
-fetch(API_URL)
-  .then((response) => response.json())
-  .then((result) => {
-    result.filter((obj) => {
-      if (obj.company.name === "Johns Group") return console.log(obj);
-    });
-  })
-  .catch((responseError) => {
-    if ((responseError.code = 404)) {
-      console.error("Address is not found");
-    } else {
-      console.error(responseError);
-    }
-  });
+// fetch(API_URL)
+//   .then((response) => response.json())
+//   .then((result) => {
+//     result.filter((obj) => {
+//       if (obj.company.name === "Johns Group") return console.log(obj);
+//     });
+//   })
+//   .catch((responseError) => {
+//     if ((responseError.code = 404)) {
+//       console.error("Address is not found");
+//     } else {
+//       console.error(responseError);
+//     }
+//   });
+
+// async.await.---------------------/
+
+// const getUser = async () => {
+//   try {
+//     const response = await fetch(API_URL);
+//     if (!response.ok) {
+//       throw response;
+//     }
+//     const data = await response.json();
+//     data.filter((obj) => {
+//       if (obj.company.name === "Johns Group") console.log(obj);
+//     });
+//   } catch (responseError) {
+//     if ((responseError.code = 404)) {
+//       console.error("Address is not found");
+//     } else {
+//       console.error(responseError);
+//     }
+//   }
+// };
+// getUser();
